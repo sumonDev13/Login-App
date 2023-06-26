fdc2a481198006e48b3dea14abf700e14eb4255d
sumon <sumonmondal1530765@gmail.com>
2023-06-26 18:59:36 +0600

    tailwind css added

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        <h1>Login</h1>
-      </div>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   );
 }
