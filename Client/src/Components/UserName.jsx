5e266cec75e1d4dcd30ac439fbe3503b3fc0ee10
sumon <sumonmondal1530765@gmail.com>
2023-06-27 19:06:47 +0600

    WIP-loginPAge

--- Client/src/Components/UserName.jsx
+++ Client/src/Components/UserName.jsx
@@ -1,15 +1,32 @@
+import {Link} from 'react-router-dom';
+import avatar from '../assets/profile.png'
+
+import Styles from '../Components/Styles/Username.module.css'
 
 
 const Username = () => {
   return (
     <div className="container mx-auto">
       <div className="flex justify-center items-center h-screen">
-        <div>
+        <div className={Styles.glass}>
           <div className="title flex flex-col items-center">
               <h4 className="text-5xl font-bold">Hello Again !</h4>
               <span className="py-4 text-xl w--2/3 text-center text-gray-500">Explore More by connecting with us</span>
           </div>
-          <form >
+          <form className="py-1">
+            <div className="profile flex justify-center py-4">
+              <img src={avatar} className={Styles.profile_img} alt="avatar"></img>
+
+            </div>
+
+            <div className="textbox flex flex-col items-center gap-6">
+                <input className={Styles.textbox} type="text" placeholder="Username"></input>
+                <button className={Styles.btn} type="submit">Lets Go</button>
+            </div>
+
+            <div className="text-center py-4">
+                <span className="text-gray-500">Not a member<Link className='text-red-500' to="/register">Register Now</Link></span>
+            </div>
 
           </form>
         </div>
