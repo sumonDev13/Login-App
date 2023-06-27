cd1bb67a14d495409bfbf5091ee6c09e33153ca0
sumon <sumonmondal1530765@gmail.com>
2023-06-27 18:29:36 +0600

    login -form-WIP

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-import Username from  './Components/UserName'
+import Username from "./Components/UserName";
 import Password from './Components/Password'
 import Register from './Components/Register'
 import Profile from './Components/Profile'
 import Recovery from './Components/Recovery'
 import Reset from './Components/Reset'
 import PageNotFound from "./Components/PageNotFound";
+
 
 
 /*root routes  */
@@ -46,9 +47,6 @@ function App() {
   return (
     <main>
       <RouterProvider router={router}></RouterProvider>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
     </main>
   );
 }
--- Client/src/Components/UserName.jsx
+++ Client/src/Components/UserName.jsx
@@ -1,9 +1,21 @@
 
 
-const UserName = () => {
+const Username = () => {
   return (
-    <div>UserName</div>
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div>
+          <div className="title flex flex-col items-center">
+              <h4 className="text-5xl font-bold">Hello Again !</h4>
+              <span className="py-4 text-xl w--2/3 text-center text-gray-500">Explore More by connecting with us</span>
+          </div>
+          <form >
+
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default UserName
+export default Username
