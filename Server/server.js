058a57bc6e86ac79de75f0f15a34b84c8bf61b6b
sumon <sumonmondal1530765@gmail.com>
2023-06-30 01:36:18 +0600

    database connected

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 import  express from 'express';
 import cors from 'cors';
 import morgan from 'morgan'
+import connect from './database/conn.js'
 
 const app = express();
 
@@ -24,8 +25,16 @@ app.get('/', (req, res) => {
 })
 
 
-/**Server listening..... */
-
-app.listen(port , ()=> {
-    console.log(`server started on http://localhost:${port}`)
+/** start server only when we have valid connection */
+connect().then(() => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server connected to http://localhost:${port}`);
+        })
+    } catch (error) {
+        console.log('Cannot connect to the server')
+    }
+}).catch(error => {
+    console.log("Invalid database connection...!");
 })
+
